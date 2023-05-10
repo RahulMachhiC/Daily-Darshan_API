869be5cbdaa763656e9547dd6b8f229aa17aa0a9
RahulMachhiC <64777899+RahulMachhiC@users.noreply.github.com>
2023-05-10 21:27:57 +0530

    Update db.js

--- connection/db.js
+++ connection/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('hksoftwe_idd', 'hksoftwe', 'p7]8tzVE+13yVH', {
+const sequelize = new Sequelize('', '', '', {
     host: 'localhost',
     dialect: 'mysql',
     timezone: '+05:30', // set timezone to Asia/Kolkata
